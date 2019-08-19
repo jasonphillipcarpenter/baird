d1a02c3201eec8084a5461d48671bc0bbcc73629
boweevil <argonaut.linux@gmail.com>
2019-08-18 23:46:01 -0400

    Add class for handling TMUX window

---
+++ src/baird/tmux_window.py
@@ -0,0 +1,15 @@
+"""Create a new TMUX window"""
+
+
+class TmuxWindow:
+    """Define a TMUX window"""
+    def __init__(self, session, args):
+        self.session = session
+        self.args = args
+
+    def return_new_window(self):
+        """
+        Return new TMUX window
+        """
+        return self.session.new_window(
+            attach=True, window_name=self.args.title)
