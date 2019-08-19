1edcf200d1ec1c08e33e69d984fa81fb23debd80
boweevil <argonaut.linux@gmail.com>
2019-08-18 23:47:30 -0400

    Add main function

---
+++ src/baird/main.py
@@ -0,0 +1,40 @@
+"""Connect to multiple servers using tmux"""
+import subprocess
+import textwrap
+import argparse
+import libtmux
+from baird.cli import Cli
+from baird.ssh_cmd import SSHCmd
+from baird.tmux_session import TmuxSession
+from baird.tmux_window import TmuxWindow
+from baird.connections import Connections
+
+
+def main():
+    """Connect to multiple servers using tmux"""
+    parser = Cli(
+        argparse,
+        textwrap
+    )
+    args = parser.return_parser()
+    ssh_cmd = SSHCmd(
+        args
+    ).return_ssh_cmd()
+    tmux_session = TmuxSession(
+        subprocess,
+        libtmux,
+        args
+    ).get_session()
+    tmux_window = TmuxWindow(
+        tmux_session,
+        args
+    ).return_new_window()
+    Connections(
+        tmux_window,
+        ssh_cmd,
+        args
+    ).create_connections()
+
+
+if __name__ == "__main__":
+    main()
